5c1eb53818af7fee71ada3bb45956562f5c48fbd
shanelevites <srlevit94@gmail.com>
2023-01-10 23:14:35 -0800

    view roles, employees, and departments properly run

--- schema.sql
+++ schema.sql
@@ -3,27 +3,31 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
-   id INT NOT NULL,
-   name VARCHAR(30) NOT NULL
+SELECT DATABASE();
+
+CREATE TABLE departments (
+   id INT NOT NULL AUTO_INCREMENT,
+   name VARCHAR(30) NOT NULL,
   -- adds an additional constraint, cannot enter two rows with the same value --
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-  id INT NOT NULL,
-  title INT NOT NULL,
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
-  id INT NOT NULL,
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
+  role_id INT,
   manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // packages needed for this application
-const inquirer = require("inquirer");
 const express = require('express');
+const inquirer = require("inquirer");
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3004;
+
+const PORT = process.env.PORT || 3005;
 const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -18,3 +23,199 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the company_db database.`)
   );
+
+// Default response for any other request (Not Found)
+// ref 11-INs
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const mainMenu = async () => {
+  return inquirer.prompt ([
+  {   type: 'list',
+      message: "What would you like to do?",
+      name: 'main',
+      default:  'Sample title',
+      choices: [
+          'Add employee',
+          'Update Employee Role',
+          'View All Roles',
+          'Add Role',
+          'View all Departments',
+          'Add Department',
+          'View All Employees',
+          'Quit'
+      ]
+  }
+  ])
+  .then((response) => {
+    switch (response.main) {
+      case 'Add employee':
+        addEmployee();
+        break;
+
+      case 'Update Employee Role':
+        updateEmployeeRole();
+        break;
+            
+      case 'View All Roles':
+        console.log('now showing roles');
+        viewAllRoles();
+        break;
+
+      case 'Add Role':
+        addRole();
+        break;
+
+      case 'View all Departments':
+        viewAllDepartments();
+        break;
+
+      case 'Add Department':
+        addDepartment();
+        break;
+
+      case 'View All Employees':
+        viewAllEmployees();
+        break;
+            
+      case 'Quit':
+      default: db.end();
+    }
+  })
+}
+
+// If View All Depts
+// show table with department names and department ids
+
+const viewAllDepartments = () => {
+  db.query('SELECT * FROM departments', function (err, results) {
+    console.table(results);
+    mainMenu();
+  });
+  
+}
+
+// If View All roles
+// show table with job title, role id, the department that role belongs to, and the salary for that role
+const viewAllRoles = async () => {
+  db.query('SELECT * FROM roles', function (err, results) {
+    console.table(results);
+    mainMenu();
+  });
+  
+}
+
+// If View All Employees
+// show table with employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+const viewAllEmployees = () => {
+    db.query('SELECT * FROM employees', function (err, results) {
+      console.table(results);
+      mainMenu();
+    });
+    
+}
+
+// If Add Emmployee 
+// // Add Employee, log message to console
+
+const addEmployee = async () => {
+  return inquirer.prompt([
+      {
+          type: "input",
+          message: "Please enter employee's first name",
+          name: "first-name"
+          // when: (data) => data.role !== "None, build my page!"
+      },
+      {
+          type: "input",
+          message: "Please enter employee's last name",
+          name: "first-name"
+          // when: (data) => data.role !== "None, build my page!"
+      },
+      {
+          type: "input",
+          message: "Please enter employee's manager",
+          name: "manager"
+          // when: (data) => data.role !== "None, build my page!"
+      },
+  ])
+}
+
+// If Add Deptartment
+// // What is the department name?
+// // Add Department, log message to console
+const addDepartment = async () => {
+
+  return inquirer.prompt([
+      {
+          type: "input",
+          message: "What is the new department's name",
+          name: "dept-name"
+          // when: (data) => data.role !== "None, build my page!"
+      }
+  ])
+}
+
+// If Add Role
+// // What is the name of the role?
+// // What is the salary of the role?
+// //  Which dept does the role belong to?
+// // Add role, log message to console
+
+const addRole = async () => {
+
+  return inquirer.prompt([
+      {
+          type: "input",
+          message: "Please enter the new role's name",
+          name: "role-name"
+          // when: (data) => data.role !== "None, build my page!"
+      },
+      {
+          type: "input", //choice?
+          message: "What is the salary of the new role?",
+          name: "role-salary",
+          // Validate Integer value
+          // when: (data) => data.role !== "None, build my page!"
+      },
+      {
+          type: "input", // choice?
+          message: "Which department does the role belong to?",
+          name: "role-dept"
+          // when: (data) => data.role !== "None, build my page!"
+      }
+  ])
+}
+
+// If Update Employee Role
+// // show table, select employee, update their role
+
+const updateEmployeeRole = async () => {
+
+  // show roster table of employees
+
+  return inquirer.prompt([
+      {
+          type: "input", // choice ?
+          message: "Please select an employee whose role you wish to change.",
+          name: "employee-to-update"
+          // when: (data) => data.role !== "None, build my page!"
+      },
+      {
+          type: "input", //choice?
+          message: "Please select a new role for the employee.",
+          name: "new-role",
+          // Validate Integer value
+          // when: (data) => data.role !== "None, build my page!"
+      }
+  ])
+}
+
+
+mainMenu();
